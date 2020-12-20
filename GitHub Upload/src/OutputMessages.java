d088fdea8ac04af0b1f5f5fe43ee6885b6f13455
Matthew <Matthew@DESKTOP-G19811A>
2020-12-20 12:01:53 +0000

    System output messages

---
+++ GitHub Upload/src/OutputMessages.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Matthew
+ *
+ */
+public class OutputMessages {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Message to GitHub");
+
+	}
+
+}
